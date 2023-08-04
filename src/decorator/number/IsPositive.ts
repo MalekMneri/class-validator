b555a1bfcccf6945052f160c4665634f84ce0b78
Malek Mneri <mmneri@anypli.com>
2023-08-04 10:13:01 +0100

    allows zeros option in IsPositive Decorator

--- src/decorator/number/IsPositive.ts
+++ src/decorator/number/IsPositive.ts
@@ -6,19 +6,25 @@ export const IS_POSITIVE = 'isPositive';
 /**
  * Checks if the value is a positive number greater than zero.
  */
-export function isPositive(value: unknown): boolean {
-  return typeof value === 'number' && value > 0;
+export function isPositive(value: unknown, options: { allowsZeros: boolean }): boolean {
+  const { allowsZeros } = options;
+  if (typeof value !== 'number') {
+    return false;
+  }
+  return allowsZeros ? value >= 0 : value > 0;
 }
 
 /**
  * Checks if the value is a positive number greater than zero.
+ *
+ * To allow zeros, you can specify allowsZeros as true
  */
-export function IsPositive(validationOptions?: ValidationOptions): PropertyDecorator {
+export function IsPositive({ allowsZeros = false } = {}, validationOptions?: ValidationOptions): PropertyDecorator {
   return ValidateBy(
     {
       name: IS_POSITIVE,
       validator: {
-        validate: (value, args): boolean => isPositive(value),
+        validate: (value, args): boolean => isPositive(value, { allowsZeros }),
         defaultMessage: buildMessage(
           eachPrefix => eachPrefix + '$property must be a positive number',
           validationOptions
